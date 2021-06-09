1a0f376b38691c8034dbf794073d47663d8062c0
chiugui <59639733+chiugui@users.noreply.github.com>
2021-06-09 16:29:57 +0800

    Update config.py

--- config.py
+++ config.py
@@ -93,5 +93,6 @@ CONFIG = {
                                                             #       error(错误信息)                    
     },
     'server_token': 'token_to_request_ddns_server_api'      # 请求DDSN server api的token
-    'server_port': 7788                                     # DDNS server 端口
+    'server_port': 7788
+    # DDNS server 端口
 }
